ec8d7d6a97acffd60ebb693ce0163572059ddbf5
castynet <riungemaina@gmail.com>
2021-04-18 20:41:02 +0300

    components/frontPage: moved styling to modular

--- components/frontPage.jsx
+++ components/frontPage.jsx
@@ -1,55 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import styles from "../styles/frontPage.module.css";
 
 export default function frontPage() {
+  const s = styles;
+
   return (
     <>
-      <Wrapper>
-        <p>the finest talent across Kenya</p>
-        <button>See the Talent</button>
+      <div className={s.wrapper}>
+        <p className={s.introText}>the finest talent across Kenya</p>
+        <button className={s.button}>See the Talent</button>
         <div>
-          <button>Login</button>
-          <button>Signup</button>
+          <button className={s.button}>Login</button>
+          <button className={s.button}>Signup</button>
         </div>
-      </Wrapper>
+      </div>
     </>
   );
 }
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background-size: cover;
-  background-color: #000;
-  font-family: "Unica One", cursive;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  flex-wrap: wrap;
-
-  &:before {
-    content: " ";
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0.04;
-    background-image: url("https://image.freepik.com/free-photo/kenya-flag-ruffled-beautifully-waving-macro-close-up-shot_1379-77.jpg");
-    background-repeat: no-repeat;
-    background-position: 50% 0;
-    background-size: cover;
-  }
-  p {
-    color: rgb(239 249 255);
-    font-size: 80px;
-    text-align: center;
-    padding: 200px 0 0 0;
-  }
-  button {
-    background: rgb(239 249 255);
-    height: 30px;
-    margin: 20px;
-    text-align: center;
-  }
-`;
