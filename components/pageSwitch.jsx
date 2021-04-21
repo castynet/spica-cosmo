331c025d2ef153d956df13dda6c64c5e6fda0b07
castynet <riungemaina@gmail.com>
2021-04-21 07:42:56 +0300

    components/pageSwitch: created & added markup

---
+++ components/pageSwitch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function () {
+  return (
+    <>
+      <div className="layer">
+        <div className="layer layerOne"></div>
+        <div className="layer layerTwo"></div>
+      </div>
+    </>
+  );
+}
