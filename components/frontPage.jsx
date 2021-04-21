232bd11067b9febb1edaf47c21f96fc29d16d589
castynet <riungemaina@gmail.com>
2021-04-21 04:25:04 +0300

    components/frontPage: added classes to the normal buttons

--- components/frontPage.jsx
+++ components/frontPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../styles/frontPage.module.css";
+import { Icons } from "./staticUtilities";
 
 export default function frontPage() {
   const s = styles;
@@ -7,11 +8,19 @@ export default function frontPage() {
   return (
     <>
       <div className={s.wrapper}>
-        <p className={s.introText}>the finest talent across Kenya</p>
-        <button className={s.button}>See the Talent</button>
+        <p className={s.introText}>showcasing the finest talent across Kenya</p>
+        <button className={`${s.button} ${s.homeButtonBig}`}>
+          See the Talent
+        </button>
         <div>
-          <button className={s.button}>Login</button>
-          <button className={s.button}>Signup</button>
+          <button className={`${s.button} ${s.homeButtonNormal}`}>
+            Sign-in &nbsp;
+            <Icons.Lock size="23" />
+          </button>
+          <button className={`${s.button} ${s.homeButtonNormal}`}>
+            Sign-up &nbsp;
+            <Icons.LockOpen size="23" />
+          </button>
         </div>
       </div>
     </>
