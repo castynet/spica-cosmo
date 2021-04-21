9c1e6112086383a80ea25d1d7d7ef48c2220bfd0
castynet <riungemaina@gmail.com>
2021-04-21 07:42:45 +0300

    components/engine: added switch page logic

--- components/engine.js
+++ components/engine.js
@@ -4,3 +4,14 @@ export function Year() {
   var n = d.getFullYear();
   return n;
 }
+
+export function SwitchPage() {
+  var layers = document.getElementsByClassName("layer");
+  for (const layer of layers) {
+    layer.classList.toggle("active");
+  }
+  const lastLayer = document.querySelector(".layerTwo");
+  lastLayer.addEventListener("transitionend", () => {
+    console.log("Transition ended");
+  });
+}
