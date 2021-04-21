01857628171d5f11a67a93dbc47b54e61c8a6bf8
castynet <riungemaina@gmail.com>
2021-04-21 04:25:27 +0300

    components/staticUtilities: added lock icons

---
+++ components/staticUtilities.js
@@ -0,0 +1,7 @@
+import { Lock } from "@styled-icons/boxicons-regular/Lock";
+import { LockOpenAlt } from "@styled-icons/boxicons-regular/LockOpenAlt";
+
+export const Icons = {
+  Lock: Lock,
+  LockOpen: LockOpenAlt,
+};
