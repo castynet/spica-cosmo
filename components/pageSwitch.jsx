586aba0a5b2e7d2d655d8c16d5c99680ecd3be48
castynet <riungemaina@gmail.com>
2021-04-22 04:21:48 +0300

    components/pageSwitch: added styles

--- components/pageSwitch.jsx
+++ components/pageSwitch.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import styles from "../styles/pageSwitch.module.css";
 
 export default function () {
+  const s = styles;
+
   return (
     <>
-      <div className="layer">
-        <div className="layer layerOne"></div>
-        <div className="layer layerTwo"></div>
+      <div data-layer="0" className={s.layer}>
+        <div data-layer="1" className={`${s.layer} ${s.layerOne}`}></div>
+        <div data-layer="2" className={`${s.layer} ${s.layerTwo}`}></div>
       </div>
     </>
   );
