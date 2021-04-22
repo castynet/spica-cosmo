2016f9e9976700535b7d01544084555ed6d21d07
castynet <riungemaina@gmail.com>
2021-04-22 04:23:57 +0300

    components/staticUtilities: documentation

--- components/staticUtilities.js
+++ components/staticUtilities.js
@@ -2,6 +2,7 @@ import { Lock } from "@styled-icons/boxicons-regular/Lock";
 import { LockOpenAlt } from "@styled-icons/boxicons-regular/LockOpenAlt";
 
 export const Icons = {
+  // used by => | 1. FrontPage |
   Lock: Lock,
   LockOpen: LockOpenAlt,
 };
