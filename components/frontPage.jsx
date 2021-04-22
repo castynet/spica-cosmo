b474e834b9017755ee786cc4631dd22f6fef6b79
castynet <riungemaina@gmail.com>
2021-04-22 04:23:28 +0300

    components/frontPage: imported & configured page switch animation

--- components/frontPage.jsx
+++ components/frontPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "../styles/frontPage.module.css";
+
+import PageSwitch from "./pageSwitch";
 import { Icons } from "./staticUtilities";
+import { SwitchPage } from "./engine";
 
 export default function frontPage() {
   const s = styles;
@@ -9,7 +12,10 @@ export default function frontPage() {
     <>
       <div className={s.wrapper}>
         <p className={s.introText}>showcasing the finest talent across Kenya</p>
-        <button className={`${s.button} ${s.homeButtonBig}`}>
+        <button
+          onClick={SwitchPage}
+          className={`${s.button} ${s.homeButtonBig}`}
+        >
           See the Talent
         </button>
         <div>
@@ -23,6 +29,7 @@ export default function frontPage() {
           </button>
         </div>
       </div>
+      <PageSwitch />
     </>
   );
 }
