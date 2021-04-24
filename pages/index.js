8681cbc7ccb3f4b2aabe6b81efb0c8aa608a9d25
castynet <riungemaina@gmail.com>
2021-04-24 18:19:58 +0300

    pages/index: added view switch logic

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import Head from "next/head";
+import { useApp } from "../components/appContext";
+
 import FrontPage from "../components/frontPage";
+import PageSwitch from "../components/pageSwitch";
+import FrontPageCards from "../components/frontPageCards";
 
 export default function Home() {
+  const app = useApp();
+
+  function PageView() {
+    return app.homeView === "intro" ? <FrontPage /> : <FrontPageCards />;
+  }
+
   return (
     <>
       <Head>
         <title>Spica Cosmo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <FrontPage />
+      <PageView />
+      <PageSwitch />
     </>
   );
 }
