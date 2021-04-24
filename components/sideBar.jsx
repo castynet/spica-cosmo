32fe640e8390105f35d5d05bf28d9327dabddf2d
castynet <riungemaina@gmail.com>
2021-04-24 18:18:31 +0300

    renamed

---
+++ components/sideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/sideBar.module.css";
+
+export default function sideBar() {
+  const s = styles;
+
+  return (
+    <>
+      <div className={s.wrapper}></div>
+    </>
+  );
+}
