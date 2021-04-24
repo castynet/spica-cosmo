5eef8120e70bd0e65d20178a0c0ed39ef0876f83
castynet <riungemaina@gmail.com>
2021-04-24 18:17:50 +0300

    components/personCard: renamed

---
+++ components/personCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../styles/personCard.module.css";
+
+export default function personCards() {
+  const s = styles;
+
+  return (
+    <>
+      <div className={s.wrapper}>
+        <p>Hello World</p>
+      </div>
+    </>
+  );
+}
