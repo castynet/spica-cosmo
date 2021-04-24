0d7ef8129dbe27994ec68662854c12089b4cfa8b
castynet <riungemaina@gmail.com>
2021-04-24 18:16:25 +0300

    components/frontPage: added context & view switch logic

--- components/frontPage.jsx
+++ components/frontPage.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import styles from "../styles/frontPage.module.css";
+import { useApp } from "./appContext";
 
-import PageSwitch from "./pageSwitch";
 import { Icons } from "./staticUtilities";
 import { SwitchPage } from "./engine";
 
 export default function frontPage() {
+  const app = useApp();
   const s = styles;
+
+  function handleSwitchView() {
+    SwitchPage();
+    // the delay waits until the switch page animation is
+    // covering the screen before changing the view
+    // the sweet spot is(double || +> 300) the delay value
+    // in "../styles/pageSwitch.module.css" ->> .layer ->> transition delay
+    setTimeout(() => app.switchHomeView(), 600);
+  }
 
   return (
     <>
       <div className={s.wrapper}>
         <p className={s.introText}>showcasing the finest talent across Kenya</p>
         <button
-          onClick={SwitchPage}
+          onClick={handleSwitchView}
           className={`${s.button} ${s.homeButtonBig}`}
         >
           See the Talent
@@ -29,7 +39,6 @@ export default function frontPage() {
           </button>
         </div>
       </div>
-      <PageSwitch />
     </>
   );
 }
