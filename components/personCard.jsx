d27c64b9d85b36a86bdbdead09c6787668c880c7
castynet <riungemaina@gmail.com>
2021-05-15 21:48:07 +0300

    components/personCard: added avatar

--- components/personCard.jsx
+++ components/personCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../styles/personCard.module.css";
+import Avatar from "./avatar";
 
 export default function personCards() {
   const s = styles;
@@ -7,7 +8,9 @@ export default function personCards() {
   return (
     <>
       <div className={s.wrapper}>
-        <p>Hello World</p>
+        <Avatar className={s.avatar} alt="person avatar" />
+        <h3>James Justin</h3>
+        R - 8.5 <br /> E - 7.6 <br /> S - 3.4 <br />C - 9.1
       </div>
     </>
   );
