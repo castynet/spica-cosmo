5813f344d5625a85f6ae6bc270a02af236eb90cf
castynet <riungemaina@gmail.com>
2021-05-16 23:48:58 +0300

    components/personalPage: created

---
+++ components/personalPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/personalPage.module.css";
+
+export default function personalPage() {
+  return (
+    <>
+      <p>hello</p>
+    </>
+  );
+}
