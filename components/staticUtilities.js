e60c592396456ba232d0983cfe265723f116c5f9
castynet <riungemaina@gmail.com>
2021-05-29 00:29:27 +0300

    components/staticUtilities: imported google icon

--- components/staticUtilities.js
+++ components/staticUtilities.js
@@ -1,8 +1,10 @@
 import { Lock } from "@styled-icons/boxicons-regular/Lock";
 import { LockOpenAlt } from "@styled-icons/boxicons-regular/LockOpenAlt";
+import { Google3 } from '@styled-icons/icomoon/Google3'
 
 export const Icons = {
-  // used by => | 1. FrontPage |
+  // used by => | 1. FrontPage | 2. LoginPage
+  Google: Google3,
   Lock: Lock,
   LockOpen: LockOpenAlt,
 };
