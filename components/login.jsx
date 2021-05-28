eebf8720a6bc1f7c73e6f145e70a0f3e2bf4ae95
castynet <riungemaina@gmail.com>
2021-05-28 23:54:41 +0300

    components/login: created

---
+++ components/login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/login.module.css";
+
+export default function Login() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <form className={styles.loginWrap}></form>
+      </div>
+    </>
+  );
+}
