354c9060fbdf17cfbde18049e5a0b3a586023b8a
castynet <riungemaina@gmail.com>
2021-05-29 05:38:02 +0300

    components/staticUtilities: imported close icon

--- components/staticUtilities.js
+++ components/staticUtilities.js
@@ -1,9 +1,11 @@
 import { Lock } from "@styled-icons/boxicons-regular/Lock";
 import { LockOpenAlt } from "@styled-icons/boxicons-regular/LockOpenAlt";
-import { Google3 } from '@styled-icons/icomoon/Google3'
+import { Google3 } from '@styled-icons/icomoon/Google3';
+import { Close} from '@styled-icons/evaicons-solid/Close';
 
 export const Icons = {
   // used by => | 1. FrontPage | 2. LoginPage
+  Close: Close,
   Google: Google3,
   Lock: Lock,
   LockOpen: LockOpenAlt,
